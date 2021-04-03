f699179928472e44e04603a8beccf8ad697e4959
Sergey Komarov <59875470+MLedIum@users.noreply.github.com>
2021-04-03 16:58:01 +0300

    Update MyApp.java

--- 5th-homework/src/MyApp.java
+++ 5th-homework/src/MyApp.java
@@ -1,14 +1,7 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        Person employees[] = new Person[5];
-
-        // Заполняем массив сотрудников
-        employees[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 40000, 30);
-        employees[1] = new Person("Kazakova Alice Pavlovna", "Designer", "KazakovaMail@mail.ru", "89151374567", 70000, 26);
-        employees[2] = new Person("Ilinskaya Polina Mikhailovna", "Janitor", "Ilinskaya@rambler.ru", "89774564890", 30000, 47);
-        employees[3] = new Person("Kuznetsova Margarita Igorevna", "System Administrator", "Kuznetsova@gmail.com", "84958880001", 60000, 49);
-        employees[4] = new Person("Dementyev Arseny Alexandrovich", "Accountant", "Dementyev@gmail.com", "89261374568", 50000, 34);
+        Person[] employees = createEmployees();
 
         // Выводим в консоль сотрудников, чей возраст больше 40
         for (Person employee : employees) {
@@ -17,4 +10,17 @@ public class MyApp {
             }
         }
     }
+    
+    public static Person[] createEmployees() {
+        Person[] employees = new Person[5];
+
+        // Заполняем массив сотрудников
+        employees[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 40000, 30);
+        employees[1] = new Person("Kazakova Alice Pavlovna", "Designer", "KazakovaMail@mail.ru", "89151374567", 70000, 26);
+        employees[2] = new Person("Ilinskaya Polina Mikhailovna", "Janitor", "Ilinskaya@rambler.ru", "89774564890", 30000, 47);
+        employees[3] = new Person("Kuznetsova Margarita Igorevna", "System Administrator", "Kuznetsova@gmail.com", "84958880001", 60000, 49);
+        employees[4] = new Person("Dementyev Arseny Alexandrovich", "Accountant", "Dementyev@gmail.com", "89261374568", 50000, 34);
+        return employees;
+    }
+    
 }
