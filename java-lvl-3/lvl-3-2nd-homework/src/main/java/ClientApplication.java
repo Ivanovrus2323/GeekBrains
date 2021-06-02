7f05230f6071011f7241315334ace04010f4e6fd
Sergey Komarov <mledium@mail.ru>
2021-06-02 19:04:40 +0300

    Initial commit

---
+++ java-lvl-3/lvl-3-2nd-homework/src/main/java/ClientApplication.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import main.java.client.Client;
+
+import java.io.IOException;
+
+public class ClientApplication {
+    public static void main(String[] args) throws IOException {
+        Client.start();
+    }
+}
