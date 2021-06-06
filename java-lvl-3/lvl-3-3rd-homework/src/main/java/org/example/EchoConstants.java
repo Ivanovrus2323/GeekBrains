fc2e10bc66a06ac358ac233c57208312e5dbc6c4
mledium <mledium@mail.ru>
2021-06-06 16:48:08 +0300

    Uploaded 3rd homework init

---
+++ java-lvl-3/lvl-3-3rd-homework/src/main/java/org/example/EchoConstants.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class EchoConstants {
+    public static final String STOP_WORD = "/end";
+    public static final String HOST      = "localhost";
+    public static final int    PORT      = 8080;
+    public static final String HOST_NAME = "server";
+}
