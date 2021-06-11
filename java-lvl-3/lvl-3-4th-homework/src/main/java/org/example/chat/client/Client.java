d6004157e0d19e7ddffded1e7730bd04a32bf6c2
mledium <mledium@mail.ru>
2021-06-11 12:49:14 +0300

    Made some changes

--- java-lvl-3/lvl-3-4th-homework/src/main/java/org/example/chat/client/Client.java
+++ java-lvl-3/lvl-3-4th-homework/src/main/java/org/example/chat/client/Client.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Client extends JFrame {
